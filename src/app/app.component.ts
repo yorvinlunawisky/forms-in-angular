99823d3b19ebd726106e4c7645f7c03ce589271a
Yorvin Luna Wisky <yorvin0506@gmail.com>
2021-03-19 19:34:59 -0400

    Resetting the form.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,16 +11,49 @@ export class AppComponent {
   defaultQuestion = 'pet';
   answer = '';
   genders = ['Male', 'Female'];
+  user = {
+    username: '',
+    email: '',
+    secretQuestion: '',
+    answer: '',
+    gender: ''
+  };
+
+  submitted = false;
 
 
   suggestUserName() {
     const suggestedName = 'Superuser';
+    // this.signupForm.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     email: ''
+    //   },
+    //   secreat: 'pet',
+    //   questionAnswer: '',
+    //   gender: 'male'
+    // });
+
+    //Better approach (patchValue is only available in form).
+    this.signupForm.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    });
+
   }
 
   // onSubmit(form: NgForm) {
   //   console.log(form);
   // Alternative way}
   onSubmit(){
-    console.log(this.signupForm);
+    this.submitted = true;
+    this.user.username = this.signupForm.value.userData.username;
+    this.user.email = this.signupForm.value.userData.email;
+    this.user.secretQuestion = this.signupForm.value.secret;
+    this.user.answer = this.signupForm.value.questionAnswer;
+    this.user.gender = this. signupForm.value.gender;
+
+    this.signupForm.reset();
   }
 }
